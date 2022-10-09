f67b687efea0a3d06d39a2b5ae1f4dc1fa34f8b2
frankdev7 <franklyncr12@gmail.com>
2022-10-09 03:53:10 -0500

    interface

---
+++ IPUSHCommInterface.sol
@@ -0,0 +1,7 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+// PUSH Comm Contract Interface
+interface IPUSHCommInterface {
+    function sendNotification(address _channel, address _recipient, bytes calldata _identity) external;
+}
